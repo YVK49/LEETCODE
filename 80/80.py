4fbcfc809124c3fc9b13359c61741aa8db14cc92
vamshi <vamshikrishna8330@gmail.com>
2026-02-02 22:39:54 +0530

    remoce duplicates II

--- 80/80.py
+++ 80/80.py
@@ -1,12 +1,11 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        n= len(nums)
-        k=0
-        for i in range(0,n):
-            if k<2 or nums[i]!=nums[k-2]:
-                nums[k]=nums[i]
-                k=k+1
-        return k
-sol = Solution()
-length = sol.removeDuplicates([1,1,1,2,2,3])
-print(length)
+        i = 0
+        while i < len(nums)-2:
+            if nums[i] == nums[i+2]:
+                nums.pop(i+2)
+            else:
+                i+=1
+        return len(nums)
+
+        
